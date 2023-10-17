30b64e407a20fc314e25ef5fb1cab27caaec5c37
jeffassis <cmdtjeff@gmail.com>
2023-10-17 18:56:27 -0300

    config initial

---
+++ src/main/java/br/com/jeff/rest/exemplo/GreetingController.java
@@ -0,0 +1,20 @@
+package br.com.jeff.rest.exemplo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "Work")
+            String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
