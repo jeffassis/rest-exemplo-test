0b171a8e4879ffd4d1e85156af297fe1930d040b
jeffassis <cmdtjeff@gmail.com>
2023-10-17 18:40:43 -0300

    first commit

---
+++ src/test/java/br/com/jeff/rest/exemplo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeff.rest.exemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
